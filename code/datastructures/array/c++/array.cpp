dab1023a95dfa4f7b368c792162ae6aaa78e1dcf
Drew Gotshalk <32356221+The-Black-Star@users.noreply.github.com>
2020-08-05 22:46:25 -1000

    starting array

---
+++ code/datastructures/array/c++/array.cpp
@@ -0,0 +1,39 @@
+//array
+//Drew Gotshalk
+//2020-08-05
+#include <iostream>
+#include <string>
+#include "array.h"
+
+template <class T>
+int my_array<T>::size(){}
+
+template <class T>
+int my_array<T>::capacity(){}
+
+template <class T>
+bool my_array<T>::is_empty(){}
+
+template <class T>
+T my_array<T>::at(int index){}
+
+template <class T>
+void my_array<T>::push(T item){}
+
+template <class T>
+void my_array<T>::insert(int index, T item){}
+
+template <class T>
+void my_array<T>::prepend(T item){}
+
+template <class T>
+T my_array<T>::pop(){}
+
+template <class T>
+void my_array<T>::remove(int index){}
+
+template <class T>
+void my_array<T>::remove_val(T val){}
+
+template <class T>
+int my_array<T>::fine(T val){}
